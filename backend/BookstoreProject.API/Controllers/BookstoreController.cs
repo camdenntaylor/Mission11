a221fc8f98fbf0da4aa34372392f3eedb09ebe9b
Camden <camden.c.taylor@gmail.com>
2025-03-24 17:07:02 -0600

    Initial for phase03

--- backend/BookstoreProject.API/Controllers/BookstoreController.cs
+++ backend/BookstoreProject.API/Controllers/BookstoreController.cs
@@ -4,19 +4,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookstoreProject.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class BookstoreController : ControllerBase
     {
         private BookstoreDbContext _bookstoreContext;
-        public BookstoreController(BookstoreDbContext temp) 
-        { 
+        public BookstoreController(BookstoreDbContext temp)
+        {
             _bookstoreContext = temp;
         }
 
-        public IActionResult GetBooks(int pageSize = 5, int pageNum = 1) 
+        [HttpGet("AllBooks")]
+        public IActionResult GetBooks(int pageSize = 5, int pageNum = 1, [FromQuery] List<string>? bookCategories = null)
         {
-            var bookStuff = _bookstoreContext.Books
+            var query = _bookstoreContext.Books.AsQueryable();
+
+            if (bookCategories != null && bookCategories.Any())
+            {
+                query = query.Where(b => bookCategories.Contains(b.Category));
+            }
+
+            var bookStuff = query
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
@@ -29,6 +37,17 @@ namespace BookstoreProject.API.Controllers
                 totalBooks = totalBooks
             });
 
+        }
+
+        [HttpGet("GetBookCategories")]
+        public IActionResult GetBookCategories()
+        {
+            var bookCategories = _bookstoreContext.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .ToList();
+
+            return Ok(bookCategories);
         }
     }
 }
