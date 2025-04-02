4dcd17c67d3aaa5f837ea11b20a68ce3ec287c0f
Camden <camden.c.taylor@gmail.com>
2025-04-01 21:20:44 -0600

    Retry

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://bookstore-project-camden-backend.azurewebsites.net/Bookstore/AllBooks/GetBookCategories'
+          'https://bookstore-project-camden-backend.azurewebsites.net/Bookstore/GetBookCategories'
         );
         const data = await response.json();
         console.log('Fetched categories:', data);
